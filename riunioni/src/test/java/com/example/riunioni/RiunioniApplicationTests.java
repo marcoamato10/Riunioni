1ed223920826638a2ac3cd91fe5844962cac680c
LuigiFerone <85681104+LuigiFerone@users.noreply.github.com>
2021-06-24 15:38:24 +0200

    Prima commit

---
+++ riunioni/src/test/java/com/example/riunioni/RiunioniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riunioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiunioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
